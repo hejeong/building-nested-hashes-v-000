5760e9db83285d3c54f03cf943ee8da1bfde1219
Jonathan Hong <jonhong777@gmail.com>
2018-07-05 22:27:01 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -24,10 +24,13 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         :Steven => {
-          
+          :name => "Steven"
+          :age => "30"
+          :attitude => "confused"
         },
         
         :Nurse => {
+          
           
         }
       ]
